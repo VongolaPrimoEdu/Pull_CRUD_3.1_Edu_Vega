e90a328fff35a63cc8ab25644a7844510d4fc9e4
VongolaPrimoEdu <eduminecraf@gmail.com>
2023-11-10 12:10:04 +0000

    Menu basico añadido que luego se añadiran los enlaces de forma adecuada

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi tienda Online</title>
+</head>
+<body>
+    <h1> Mi tienda online </h1>
+    <a href="">Nuevo Producto </a><br><br>
+
+    <a href="">Modificar Producto </a>
+    <a href="">Consultar Productos </a>
+    <a href="">Borrar productos </a>
+   
+    
+</body>
+</html>
